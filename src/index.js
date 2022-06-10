b7618a23a872572b56944e7497980522ac1843e2
lejodev <alejandro.rv97@gmail.com>
2022-06-10 12:21:31 -0500

    Routes and CRUD added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userRoute = require("./routes/user.route");
+const noteRoute = require("./routes/note.route");
 
 const app = express();
 
@@ -9,6 +10,7 @@ const PORT = process.env.port || 3000;
 
 app.use(express.json());
 app.use("/api/user/", userRoute);
+app.use("/api/note/", noteRoute);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -3,4 +3,51 @@ const noteSchema = require("../models/note.schema");
 
 const router = express.Router();
 
-router.get("/:")
+router.get("/:userId", (req, res) => {
+  const userId = req.params.userId;
+  noteSchema.find({ userId: userId }, (err, doc) => {
+    if (err) {
+      res.status(400).json({ Error: err });
+    } else {
+      res.status(200).json({ doc });
+    }
+  });
+});
+
+router.post("/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const { title, body } = req.body;
+  const note = new noteSchema({ title, body, userId });
+  note.save((err, doc) => {
+    if (err) {
+      res.status(400).json({ Error: err });
+    } else {
+      res.status(200).json({ Message: "Success", doc });
+    }
+  });
+});
+
+router.patch("/:noteId", (req, res) => {
+  const { noteId } = req.params;
+  const { title, body } = req.body;
+  noteSchema.findByIdAndUpdate(noteId, { title, body }, (err, doc) => {
+    if (err) {
+      res.status(400).json({ Error: err });
+    } else {
+      res.status(200).json({ Message: "Success" });
+    }
+  });
+});
+
+router.delete("/:noteId", (req, res) => {
+  const noteId = req.params.noteId;
+  noteSchema.findOneAndDelete({ _id: noteId }, (err, doc) => {
+    if (err) {
+      res.status(400).json({ Error: err });
+    } else {
+      res.status(200).json({ Message: "Deleted successfully" });
+    }
+  });
+});
+
+module.exports = router;
